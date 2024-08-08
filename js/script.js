d5f86b780a29adb3576e0e4cfd0a790d659fb1f9
Reuben Humphrey <reuben@rjh.nz>
2024-08-08 14:15:14 +1200

    random little things

--- js/script.js
+++ js/script.js
@@ -10,7 +10,7 @@ let pos1 = 0,
 	cursorX = 0,
 	cursorY = 0;
 
-let ktkaValue = 0;
+let ktkaValue = 50;
 let backgroundLvl = 1;
 
 let menuState = "";
@@ -31,7 +31,6 @@ window.onload = function () {
 	makeGrid();
 	makeMap();
 };
-
 
 setInterval(function checkKtkaLevel () {
 	let ktkaBarFill = document.getElementById("fill");
@@ -133,14 +132,4 @@ function addToCell (x, y, ...rest) {
 		}
 		styleElement.sheet.insertRule(`#map > div:nth-child(${cellIndex + 1}) ${rest[i][2]}`);
 	}
-}
-
-function makeMap () {
-	addToCell(1, 1, ["img", ["src", "images/building6.png", "id", "testImgId", "class", "building-image"],
-		`img {
-			height: 140px;
-			width: 140px;
-			margin: 5px;
-		}`]
-	);
 }
